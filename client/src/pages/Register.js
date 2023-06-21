21ccf539188ca47c9de1858c67dde1de789cfc05
subhadippaik12 <126114327+subhadippaik12@users.noreply.github.com>
2023-06-21 18:14:08 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -24,7 +24,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("/api/v1/user/register", {
+      const { data } = await axios.post("https://blogly-backend.onrender.com/api/v1/user/register", {
         username: inputs.name,
         email: inputs.email,
         password: inputs.password,
