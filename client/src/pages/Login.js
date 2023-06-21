bfa9f9633a84f8ec8b2112ec3f013d5a3dfdbdbd
subhadippaik12 <126114327+subhadippaik12@users.noreply.github.com>
2023-06-21 18:12:11 +0530

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -26,7 +26,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("/api/v1/user/login", {
+      const { data } = await axios.post("https://blogly-backend.onrender.com/api/v1/user/login", {
         email: inputs.email,
         password: inputs.password,
       });
